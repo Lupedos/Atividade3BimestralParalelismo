000aacdd894614986c7fb330e29407536b14bd59
Lupedos <68112583+Lupedos@users.noreply.github.com>
2023-05-01 21:14:33 -0300

    r

--- src/Atividade3BimestralParalelismo/Main.java
+++ src/Atividade3BimestralParalelismo/Main.java
@@ -1,16 +1,5 @@
 package Atividade3BimestralParalelismo;
-<<<<<<< HEAD
 
-
-
- 
-
-public class Main {
-    public static void main (String[] args) {
-        new Program(5,8);
-    }
-}
-=======
 public class Main {
 //O programa deve primeiramente criar uma instância única da classe Prédio
 	// a qual cria um objeto do tipo Elevador e o inicializa
@@ -20,4 +9,4 @@ public class Main {
 	
 	}
 }
->>>>>>> parent of 28382c3 (teste mas nao funfa)
+
